25fd72821bf74b4ee872252a4f642b956d7c0dea
Jiří Slíva <jiri.sliva@gmail.com>
2023-03-28 18:50:31 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,79 @@
-function usmev3 () {
+function smutek () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+}
+function nevolnost () {
+    timer = 0
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . . . .
+        . # # # .
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+}
+function hello () {
+    timer = 0
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+}
+input.onGesture(Gesture.Shake, function () {
+    nevolnost()
+})
+function happy () {
+    timer = 0
     basic.showLeds(`
         . # . # .
         . # . # .
         . . . . .
         # . . . #
-        # # # # #
-        `)
-}
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    usmev3()
-})
-function usmev2 () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # # # # #
         . # # # .
         `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
 }
 function spanek () {
     basic.showLeds(`
         . . . . .
         # # . # #
         . . . . .
-        . . . . .
         . # # # .
+        . . . . .
         `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
 }
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    usmev1()
+    happy()
 })
-function usmev1 () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
+function dead () {
+    basic.showIcon(IconNames.Skull)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
 }
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    usmev2()
-})
-input.onLogoEvent(TouchButtonEvent.Released, function () {
-    spanek()
-})
-spanek()
+let timer = 0
+timer = 0
+hello()
 basic.forever(function () {
-	
+    basic.pause(1000)
+    timer += 1
+    if (timer == 20) {
+        smutek()
+    }
+    if (timer == 30) {
+        spanek()
+    }
+    if (timer == 40) {
+        dead()
+    }
 })
